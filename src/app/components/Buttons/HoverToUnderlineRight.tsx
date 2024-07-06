f0e638b8c38d42baae856dde39b5bd391fd14ca3
HikariTakahashi <166400513+HikariTakahashi@users.noreply.github.com>
2024-07-06 17:04:19 +0900

    ホバーするとアンダーラインが右からフェードインするボタンの追加

---
+++ src/app/components/Buttons/HoverToUnderlineRight.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Box, Link } from "@mui/material";
+import { useState, useEffect } from "react";
+import { keyframes } from "@emotion/react";
+
+type TProps = {
+  ButtonName: string;
+};
+
+const fadeIn = keyframes`
+  from {
+    transform: scaleX(0);
+  }
+  to {
+    transform: scaleX(1);
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    transform: scaleX(1);
+  }
+  to {
+    transform: scaleX(0);
+  }
+`;
+
+export const HoverToUnderlineRight: React.FC<TProps> = ({ ButtonName }) => {
+  const [hover, setHover] = useState(false);
+  const [visibility, setVisibility] = useState("hidden");
+
+  useEffect(() => {
+    if (hover) {
+      setVisibility("visible");
+    } else {
+      const endHover = setTimeout(() => setVisibility("hidden"), 300);
+      return () => clearTimeout(endHover);
+    }
+  }, [hover]);
+
+  return (
+    <Box
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <Link
+        href="/"
+        underline="none"
+        color={"black"}
+        p={1}
+        fontSize={21}
+        sx={{ cursor: "pointer" }}
+      >
+        {ButtonName}
+      </Link>
+      <Box
+        width={82}
+        height={2}
+        bgcolor="black"
+        sx={{
+          visibility: visibility,
+          transformOrigin: "right",
+          animation: `${hover ? fadeIn : fadeOut} 0.3s ease-in-out`,
+        }}
+      />
+    </Box>
+  );
+};
+
+export default HoverToUnderlineRight;
