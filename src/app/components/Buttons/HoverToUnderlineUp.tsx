a6f0697e524b377f4a7ff706ae6022675a9bfa5e
HikariTakahashi <166400513+HikariTakahashi@users.noreply.github.com>
2024-07-06 11:01:11 +0900

    ホバーするとアンダーラインが表れるボタンの追加

---
+++ src/app/components/Buttons/HoverToUnderlineUp.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Box, Link } from "@mui/material";
+import { useState } from "react";
+import { keyframes } from "@emotion/react";
+
+type TProps = {
+  ButtonName: string;
+};
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(8px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+export const HoverToUnderlineUp: React.FC<TProps> = ({ ButtonName }) => {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <Box
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <Link
+        href="/"
+        underline="none"
+        color={"black"}
+        p={1}
+        fontSize={21}
+        sx={{ cursor: "pointer" }}
+      >
+        {ButtonName}
+      </Link>
+      <Box
+        width={80}
+        height={2}
+        bgcolor="black"
+        sx={{
+          visibility: hover ? "visible" : "hidden",
+          animation: hover ? `${fadeIn} 0.3s ease-in-out` : "none",
+        }}
+      />
+    </Box>
+  );
+};
+
+export default HoverToUnderlineUp;
